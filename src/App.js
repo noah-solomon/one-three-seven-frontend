4b84bcac3d9a9fb91a136760c4864f43940947b5
Noah Solomon <njs99@cornell.edu>
2022-01-11 01:23:55 -0700

    Add basic networking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import Column from './Column';
-var url = "https://ots-tasks-backend.herokuapp.com";
-function App() {
-  const getTasks = async () => {
-    const response = await fetch(url + '/api/tasks/');
-    const response_json = await response.json();
-    return response_json.data;
-  };
-  getTasks().then(data => {
+import React from 'react';
+
+
+const url = "https://ots-tasks-backend.herokuapp.com";
+
+const getTasks = async () => {
+  const response = await fetch(url + '/api/tasks/');
+  const response_json = await response.json();
+  return response_json.data;
+};
+
+class App extends React.Component {
+
+
+  componentDidMount() {
+    getTasks().then((data) => {
+      this.setState({ tasks: data });
+    });
+  }
+
+  render() {
     return (
-      <div className="App">
-        <Column title="Today" tasks={data.filter(d => d.column_name = "one")}>
-        </Column>
-        <Column title="3 days" tasks={data.filter(d => d.column_name = "three")} />
-        <Column title="7 days" tasks={data.filter(d => d.column_name = "seven")} />
+      <div>
+        <h1>{'Loading...'}</h1>
+        {this.state && this.state.tasks &&
+          <div className="App">
+            <Column title="Today" tasks={this.state.tasks.filter(d => d.column_name === "one")}>
+            </Column>
+            <Column title="3 days" tasks={this.state.tasks.filter(d => d.column_name === "three")} />
+            <Column title="7 days" tasks={this.state.tasks.filter(d => d.column_name === "seven")} />
+          </div>
+        }
       </div>
-    );
-  });
+    )
+  }
 }
 
 export default App;
